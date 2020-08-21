8173b5d7748090df21123c8174890d00a09db7bb
Zach Corpuz <zachcorpuz@yahoo.com>
2020-08-21 15:36:05 -0700

    Created base footer. Refactored syntax to make it JSX. Rendered components to page through app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <nav className="footer fixed-bottom navbar-light bg-light">
+            <a className="footer-brand" href="#">&copy; Zachary Corpuz 2020</a>
+        </nav>
+    );
+}
+
+export default Footer;
